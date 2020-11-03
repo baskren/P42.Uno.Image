788a8d74e3d07b0cc885e6423d28e8c57df80c98
Ben Askren <ben@buildcalc.com>
2020-11-03 15:52:38 -0500

    initial commit

---
+++ Image.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P42.Uno.Image
+{
+    class Image
+    {
+    }
+}
---
+++ Image.skia.cs
@@ -0,0 +1,23 @@
+﻿#if !NETFX_CORE && !NETSTANDARD
+using SkiaSharp.Views.UWP;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+
+namespace P42.Uno.Image
+{
+    public partial class SvgImage : UserControl
+    {
+        void Build()
+        {
+            Content = new SKXamlCanvas
+            {
+
+            }
+        }
+    }
+}
+#endif
